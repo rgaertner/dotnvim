64ebb30d1ac1ae0355d665627100d46f9e74dabe
Ronny Gaertner <ronny.gaertner@googlemail.com>
2023-01-21 16:08:48 +0100

    fix: modify lsp saga setup due to breaking changes

--- lua/plugins/lsp/lspsaga.lua
+++ lua/plugins/lsp/lspsaga.lua
@@ -1,18 +1,21 @@
 -- import lspsaga safely
 local saga_status, saga = pcall(require, "lspsaga")
 if not saga_status then
-  return
+	return
 end
 
-saga.init_lsp_saga({
-  -- keybinds for navigation in lspsaga window
-  move_in_saga = { prev = "<C-k>", next = "<C-j>" },
-  -- use enter to open file with finder
-  finder_action_keys = {
-    open = "<CR>",
-  },
-  -- use enter to open file with definition preview
-  definition_action_keys = {
-    edit = "<CR>",
-  },
+saga.setup({
+	-- keybinds for navigation in lspsaga window
+	scroll_preview = {
+		scroll_down = "<C-j>",
+		scroll_up = "<C-k>",
+	},
+	-- use enter to open file with finder
+	finder = {
+		edit = { "o", "<CR>" },
+		vsplit = "s",
+		split = "i",
+		tabe = "t",
+		quit = { "q", "<ESC>" },
+	},
 })
